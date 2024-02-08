22c0332011ed27a75304497db970c3500eea30f0
christian <Christian@weinverkauf-haag.de>
2024-02-08 16:33:18 +0100

    updated tables

--- database/mysql/db/address_table.sql
+++ database/mysql/db/address_table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE `address`
 (
-  `address_id`  int          NOT NULL AUTO_INCREMENT,
-  `city`        varchar(256) NOT NULL,
-  `country`     varchar(256) NOT NULL,
-  `postal_code` varchar(256) NOT NULL,
-  `state`       varchar(256) NOT NULL,
-  `street`      varchar(256) NOT NULL,
-  `user_id`     int          NOT NULL,
+  `address_id`   int          NOT NULL AUTO_INCREMENT,
+  `city`         varchar(256) NOT NULL,
+  `country`      varchar(256) NOT NULL,
+  `postal_code`  int          NOT NULL,
+  `house_number` int          NOT NULL,
+  `state`        varchar(256) NOT NULL,
+  `street`       varchar(256) NOT NULL,
+  `user_id`      int          NOT NULL,
   PRIMARY KEY (`address_id`),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 )
