1e9682988ff11f9884867f2265c2d5fc20e4a3db
christian <Christian@weinverkauf-haag.de>
2024-02-08 14:03:55 +0100

    added address table

---
+++ database/mysql/db/address_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `address`
+(
+  `address_id`  int          NOT NULL AUTO_INCREMENT,
+  `city`        varchar(256) NOT NULL,
+  `country`     varchar(256) NOT NULL,
+  `postal_code` varchar(256) NOT NULL,
+  `state`       varchar(256) NOT NULL,
+  `street`      varchar(256) NOT NULL,
+  `user_id`     int          NOT NULL,
+  PRIMARY KEY (`address_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+)
